9e049d11b56471adfdcc2a4a04e72f38a9e40954
ayalnss <ayalounis678@gmail.com>
2025-05-11 13:48:54 +0200

    final working version

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
 
+# MongoDB setup
+connection_string = "mongodb+srv://lounisaya01:4jbuG89czpaEkvSw@cluster0.int5had.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(connection_string)
+db = client["video_analysis_db"]
+collection = db["violations"]
+
 @app.get("/")
 def read_root():
-    return {"message": "FastAPI is working!"}
+    # Test DB connection
+    try:
+        db.list_collection_names()  # Just a basic call
+        return {"message": "MongoDB connected!"}
+    except Exception as e:
+        return {"error": str(e)}
